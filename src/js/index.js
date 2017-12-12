d212f3e0c24f0ddc51c665e5213d73f33f478069
gil <gil@novius.fr>
2017-12-12 13:15:59 +0100

    setup dat.gui

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,16 @@ import '../css/style.css';
 import * as THREE from 'three';
 import 'three/OrbitControls';
 import dat from 'dat.gui/build/dat.gui.js';
-import Star from './Star.js';
 
-const stars = [];
-const star_size = 1;
-const star_count = 20;
-const distanceBetweenStars = 100;
-const randomStarPositionOffset = 200;
-const camera_distance = 20;
+const params = {
+    sphereX: 0,
+    sphereY: 0,
+    sphereZ: 0,
+    scale:   1,
+    cameraX: 0,
+    cameraY: 0,
+    cameraZ: 5,
+};
 
 // Set up the scene
 const scene = new THREE.Scene();
@@ -23,23 +25,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
-camera.position.z = camera_distance;
+controls.enableDamping = true;
+controls.dampingFactor = 0.1;
+camera.position.z = 5;
 
-// Create all stars
-for (let i = 0; i < star_count; i++) {
-    let star = new Star({
-        radius: star_size,
-        x: Math.random() * randomStarPositionOffset,
-        y: Math.random() * randomStarPositionOffset,
-        z: -(i * distanceBetweenStars),
-        scene: scene,
-    });
-    stars.push(star);
-    star.append();
-}
-
-camera.lookAt(stars[0].mesh.position);
-controls.update();
+// Let's add something to the screen!
+const geometry = new THREE.SphereGeometry(4, 32, 32);
+const material = new THREE.MeshLambertMaterial({
+    color: 0xC238B5
+});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 // And also some lights so we can see something!
 let lights = [];
@@ -57,6 +53,32 @@ function letThereBeLight () {
     }
 }
 letThereBeLight();
+
+// DAT.GUI
+const gui = new dat.GUI();
+gui.add(params, 'sphereX', -10, 10).onChange((value) => {
+    sphere.position.x = value;
+});
+gui.add(params, 'sphereY', -10, 10).onChange((value) => {
+    sphere.position.y = value;
+});
+gui.add(params, 'sphereZ', -10, 10).onChange((value) => {
+    sphere.position.z = value;
+});
+gui.add(params, 'scale', 0, 10).onChange((value) => {
+    sphere.scale.x = value;
+    sphere.scale.y = value;
+    sphere.scale.z = value;
+});
+gui.add(params, 'cameraX', -10, 10).onChange((value) => {
+    camera.position.x = value;
+});
+gui.add(params, 'cameraY', -10, 10).onChange((value) => {
+    camera.position.y = value;
+});
+gui.add(params, 'cameraZ', -10, 10).onChange((value) => {
+    camera.position.z = value;
+});
 
 // Those line make sure that we maintain a correct aspect ratio when resizing window
 window.addEventListener('resize', () => {
